263e211cad2a442bbf70391a68d2d7ee1d35b14a
Adrian Kunz <a.kunz@uni-kassel.de>
2024-05-13 16:07:02 +0200

    test: Add failLogin test

--- src/test/java/de/uniks/stp24/controller/LoginControllerTest.java
+++ src/test/java/de/uniks/stp24/controller/LoginControllerTest.java
@@ -1,7 +1,10 @@
 package de.uniks.stp24.controller;
 
 import de.uniks.stp24.ControllerTest;
+import de.uniks.stp24.dto.ErrorResponse;
 import de.uniks.stp24.dto.LoginResult;
+import de.uniks.stp24.exception.ErrorResponseException;
+import de.uniks.stp24.service.ErrorService;
 import de.uniks.stp24.service.LoginService;
 import io.reactivex.rxjava3.core.Observable;
 import javafx.stage.Stage;
@@ -10,16 +13,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxAssert;
+import org.testfx.matcher.control.TextMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.util.WaitForAsyncUtils.waitForFxEvents;
 
 @ExtendWith(MockitoExtension.class)
 class LoginControllerTest extends ControllerTest {
     @Spy
     LoginService loginService;
+    @Spy
+    ErrorService errorService;
 
     @InjectMocks
     LoginController loginController;
@@ -53,5 +61,26 @@ class LoginControllerTest extends ControllerTest {
         // She sees the main menu.
         verify(loginService, times(1)).login("alice123", "hunter2", false);
         verify(app, times(1)).show("/main-menu");
+    }
+
+    @Test
+    void failLogin() {
+        doReturn(Observable.error(new ErrorResponseException(new ErrorResponse(401, "Forbidden", new String[]{"bad login"}))))
+            .when(loginService).login(any(), any(), anyBoolean());
+
+        // Start:
+        assertEquals("Login", stage.getTitle());
+
+        // Action:
+        write("alice123\t");
+        write("hunter3\t");
+        clickOn("Login");
+
+        waitForFxEvents();
+
+        // Result:
+        assertEquals("Login", stage.getTitle());
+        verifyThat("#errorLabel", TextMatchers.hasText("Invalid Username or Password"));
+        verify(loginService, times(1)).login("alice123", "hunter3", false);
     }
 }
